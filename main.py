8e8dd1456ade365294d2c93ae00189e92b19d008
jess305272 <tsai-jung.yu@mail.mcgill.ca>
2018-11-17 16:46:10 -0500

    chianyu fixed the webcam/everything

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, jsonify, request
+from Frontend.camera import VideoCamera
+
+
 app = Flask(__name__,
             template_folder='./Frontend/templates',
             static_folder = './Frontend/static/css'
             )
 
-
-import os
-from Frontend import camera
+video_camera = None
+global_frame = None
 
 @app.route('/')
 def hello():
@@ -19,6 +21,50 @@ def frontend():
 @app.route('/backend')
 def backend():
     return "Backend"
+
+
+@app.route('/record_status', methods=['POST'])
+def record_status():
+    global video_camera
+
+    if video_camera == None:
+        video_camera = VideoCamera()
+
+    json = request.get_json()
+
+    status = json['status']
+
+    if status == "true":
+        video_camera.start_record()
+        return jsonify(result="started")
+    else:
+        video_camera.stop_record()
+        return jsonify(result="stopped")
+
+@app.route('/video_stream')
+def video_stream():
+    global video_camera
+    global global_frame
+
+    if video_camera == None:
+        video_camera = VideoCamera()
+
+    while True:
+        frame = video_camera.get_frame()
+
+        if frame != None:
+            global_frame = frame
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+
+
+@app.route('/video_viewer')
+def video_viewer():
+    return Response(video_stream(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run()
